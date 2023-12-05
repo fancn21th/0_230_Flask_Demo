ce601c0ba3a5efe5842c2816a7fbf9093bd52d05
fancn21th <fancn21th@gmail.com>
2023-12-05 15:46:32 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+
+@app.route("/chinese")
+def hello_chinese():
+    return "你好，世界！"
+
+
+@app.route("/name/<name>")
+def hello_name(name):
+    return f"Hello, {name}!"
+
+
+# mac
+# FLASK_APP=app.py flask run
+# FLASK_APP=app.py FLASK_ENV=development flask run
+
+
+# windows
+# $env:FLASK_APP = "app.py"; flask run
+# $env:FLASK_APP = "app.py"; $env:FLASK_ENV = "development"; flask run
